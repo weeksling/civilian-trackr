1c31a091b9aeb8b780f2a6815e832dd92feec6b7
mattmili <matthewmilitante@gmail.com>
2016-04-04 04:10:54 -0400

    updated main file

--- run_trackr.py
+++ run_trackr.py
@@ -4,9 +4,12 @@ import cv2
 
 if __name__ == '__main__':
   ap = argparse.ArgumentParser()
-  ap.add_argument("-v", "--video", help="path to the video file")
+  ap.add_argument("-v", "--video", help="video file")
   args = vars(ap.parse_args())
  
-  camera = cv2.VideoCapture(args["video"])
-
-  tracker.track(camera)
+  if args["video"] is not None:
+    camera = cv2.VideoCapture(args["video"])
+    tracker.track(camera)
+  else:
+    print "USAGE: python run_trackr --video <VIDEO_FILE>"
+    ap.exit()
