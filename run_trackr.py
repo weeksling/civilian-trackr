57d4c4e898ade2893a95f2fd8deaaf51aaad60e6
mattmili <matthewmilitante@gmail.com>
2016-04-04 00:52:36 -0400

    removed webcam feature, only accepts video files

--- run_trackr.py
+++ run_trackr.py
@@ -6,15 +6,7 @@ if __name__ == '__main__':
   ap = argparse.ArgumentParser()
   ap.add_argument("-v", "--video", help="path to the video file")
   args = vars(ap.parse_args())
-
-  camera = None
-
-  # if the video argument is None, then we are reading from webcam
-  if args.get("video", None) is None:
-    camera = cv2.VideoCapture(0)
-   
-  # otherwise, we are reading from a video file
-  else:
-    camera = cv2.VideoCapture(args["video"])
+ 
+  camera = cv2.VideoCapture(args["video"])
 
   tracker.track(camera)
